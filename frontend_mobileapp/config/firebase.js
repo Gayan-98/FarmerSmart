892c8bc13a5c2d9b0b510122f7a9e47191fc1ad1
Isuru-Rangana <isururangana312@gmail.com>
2025-03-15 18:13:34 +0530

    added google auth

---
+++ frontend_mobileapp/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5GdW3VPCnfaE0H5TWJ-Je-QSNHosHtPA",
+  authDomain: "farmer-smart.firebaseapp.com",
+  projectId: "farmer-smart",
+  storageBucket: "farmer-smart.firebasestorage.app",
+  messagingSenderId: "800988493793",
+  appId: "1:800988493793:web:87b26fd82762158e55c4a0",
+  measurementId: "G-FE1845T20G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider }; 
