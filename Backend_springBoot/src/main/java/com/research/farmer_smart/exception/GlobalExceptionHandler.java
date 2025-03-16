b5e4def27f08c0576849107ad4382778f65d7a84
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-03-16 13:38:11 +0530

    add diseases detection apis

--- Backend_springBoot/src/main/java/com/research/farmer_smart/exception/GlobalExceptionHandler.java
+++ Backend_springBoot/src/main/java/com/research/farmer_smart/exception/GlobalExceptionHandler.java
@@ -9,18 +9,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.UNAUTHORIZED);
-    }
+  @ExceptionHandler(InvalidCredentialsException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(
+      InvalidCredentialsException ex) {
+    return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.UNAUTHORIZED);
+  }
 
-    @ExceptionHandler(PestInfestationException.class)
-    public ResponseEntity<ErrorResponse> handlePestInfestationException(PestInfestationException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler(PestInfestationException.class)
+  public ResponseEntity<ErrorResponse> handlePestInfestationException(PestInfestationException ex) {
+    return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+  }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+    return new ResponseEntity<>(new ErrorResponse(ex.getMessage()),
+        HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(FarmerNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleFarmerNotFoundException(RuntimeException ex) {
+    return new ResponseEntity<>(new ErrorResponse(ex.getMessage()),
+        HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(DiseasesDetectionException.class)
+  public ResponseEntity<ErrorResponse> handleDiseasesDetectionException(RuntimeException ex) {
+    return new ResponseEntity<>(new ErrorResponse(ex.getMessage()),
+        HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 } 
---
+++ Backend_springBoot/src/main/java/com/research/farmer_smart/service/serviceImpl/DiseasesDetectionServiceImpl.java
@@ -0,0 +1,58 @@
+package com.research.farmer_smart.service.serviceImpl;
+
+import com.research.farmer_smart.controller.request.DiseasesDetectionRequest;
+import com.research.farmer_smart.exception.DiseasesDetectionException;
+import com.research.farmer_smart.exception.FarmerNotFoundException;
+import com.research.farmer_smart.model.DiseasesDetection;
+import com.research.farmer_smart.model.Farmer;
+import com.research.farmer_smart.repository.DiseasesDetectionRepository;
+import com.research.farmer_smart.repository.FarmerRepository;
+import com.research.farmer_smart.service.DiseasesDetectionService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DiseasesDetectionServiceImpl implements DiseasesDetectionService {
+
+  private final FarmerRepository farmerRepository;
+  private final DiseasesDetectionRepository diseasesDetectionRepository;
+
+  @Override
+  public DiseasesDetection recordDiseasesDetection(DiseasesDetectionRequest request) {
+    try {
+      Farmer farmer = farmerRepository.findById(request.getFarmerId())
+          .orElseThrow(() -> new FarmerNotFoundException(
+              "Farmer not found with ID: " + request.getFarmerId()));
+
+      DiseasesDetection diseasesDetection = new DiseasesDetection();
+      diseasesDetection.setFarmer(farmer);
+      diseasesDetection.setDiseaseName(request.getDiseaseName());
+      diseasesDetection.setDetectedLocation(request.getDetectedLocation());
+      diseasesDetection.setDetectionDateTime(request.getDetectionDateTime());
+
+      return diseasesDetectionRepository.save(diseasesDetection);
+    } catch (Exception e) {
+      throw new DiseasesDetectionException("Error recording diseases detection: " + e.getMessage());
+    }
+  }
+
+  @Override
+  public List<DiseasesDetection> getFarmerDiseasesDetection(String farmerId) {
+    Farmer farmer = farmerRepository.findById(farmerId)
+        .orElseThrow(() -> new FarmerNotFoundException("Farmer not found"));
+    return diseasesDetectionRepository.findByFarmer(farmer);
+  }
+
+  @Override
+  public List<DiseasesDetection> searchByDiseaseName(String diseaseName) {
+    return diseasesDetectionRepository.findByDiseaseName(diseaseName);
+  }
+
+  @Override
+  public DiseasesDetection getPestInfestationById(String id) {
+    return diseasesDetectionRepository.findById(id)
+        .orElseThrow(() -> new DiseasesDetectionException(" disease detection record not found"));
+  }
+}
